f6369c53e84ebc465f588fa358286999a8bd1d3e
sinta3 <sinta.tri38@gmail.com>
2020-06-22 19:22:40 +0700

    add responsive css style

--- script.js
+++ script.js
@@ -13,6 +13,35 @@ async function gitUser() {
         let img = document.createElement("img");
         img.setAttribute("src", `${results.avatar_url}`);
         profil.appendChild(img);
+        //name dan id
+        let name = document.createElement("h4");
+        name.innerHTML = ` ${results.login}</br> ${results.id}`;
+        profil.appendChild(name);
+
+        //follower & following
+        let follower = document.getElementById("follower");
+        let following = document.getElementById("following");
+
+        follower.innerHTML = `<a href="${results.followers_url}">Followers : ${results.followers}</a>`;
+        following.innerHTML = `<a href="${results.following_url}">Following : ${results.following}</a>`;
+
+        //repo
+        let repos = document.getElementById("repos");
+        repos.innerHTML = `<a href="${results.repos_url}">Repository : ${results.public_repos}</a>`;
+        //blog
+        let blog = document.getElementById("blog");
+        blog.innerHTML = `<a href="${results.blog}">My Blog </a>`;
+        //twitter
+        let twit = document.getElementById("twit");
+        twit.innerHTML = `<a href="${results.twitter_username}">My Twitter</a>`;
+        //bio
+        let bio = document.getElementById("bio");
+        bio.innerHTML = `${results.bio}`;
+        //lokasi
+        let locate = document.getElementById("locate");
+        let loc = document.createElement("p");
+        loc.innerHTML = ` Address : ${results.location}`;
+        locate.appendChild(loc);
     } catch (error) {
         console.log(error);
     }
